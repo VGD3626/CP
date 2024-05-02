0b3af5dbc09f69f6136f3259603ca3fdc27afee3
Vrund3626 <120765596+VGD3626@users.noreply.github.com>
2024-05-02 23:10:15 +0530

    Add files via upload

---
+++ x.py
@@ -0,0 +1,13 @@
+from sys import stdin,stdout
+from math import gcd,floor,sqrt,log
+from collections import defaultdict as dd
+from bisect import bisect_left as bl,bisect_right as br
+
+intin = lambda: int(input())
+strin = lambda: input().split()
+arrin = lambda: list(map(int,input().split()))
+yn = lambda x: print('YES') if x==True else print('FALSE')
+M = 1000000007
+
+#GHOST
+
