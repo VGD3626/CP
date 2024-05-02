fd50b89eb2b99fef2e94d1e5b63c6dd48c73b18d
Vrund3626 <120765596+VGD3626@users.noreply.github.com>
2024-05-03 00:15:58 +0530

    Update and rename x.py to cp_code_template.py

--- cp_code_template.py
+++ cp_code_template.py
@@ -4,10 +4,10 @@ from collections import defaultdict as dd
 from bisect import bisect_left as bl,bisect_right as br
 
 intin = lambda: int(input())
-strin = lambda: input().split()
-arrin = lambda: list(map(int,input().split()))
-yn = lambda x: print('YES') if x==True else print('FALSE')
+seqin = lambda: list(map(int,input().split()))
+yn = lambda x:  print('YNEOS'[x::2])
 M = 1000000007
 
 #GHOST
-
+t = 1 #intin()
+for _ in range(t):
